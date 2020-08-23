685870ff05f94b31f2bf5074900cf75283d872fa
Carol Schulze <carol@ereski.org>
2020-08-23 16:27:33 -0300

    Add an example for an async trait method that takes a lifetime argument

--- src/tests.rs
+++ src/tests.rs
@@ -5,8 +5,14 @@ use crate::{
     sync::H1Arc,
     Rcb, StreamingIterator,
 };
-use std::rc::Rc;
-use std::sync::Arc;
+use async_executor::LocalExecutor;
+use std::{
+    future::Future,
+    pin::Pin,
+    rc::Rc,
+    sync::Arc,
+    task::{Context, Poll},
+};
 
 #[test]
 fn self_borrowing_iterator() {
@@ -122,4 +128,75 @@ fn struct_using_either_rc_or_arc_with_hkt() {
         StructWithReferenceCount::<H1Arc>::new().clone().secrets(),
         ("xpotato", 42)
     );
+}
+
+// Non-trivial async-like trait method. Has the advantage that there's no need
+// to box the resulting future (like https://crates.io/crates/async-trait), but
+// has the disadvantage of making the trait object-unsafe (can't be used as
+// `dyn`). The implementation here is very manual and can probably be mostly
+// abstracted by a macro.
+//
+// Also see:
+// https://smallcultfollowing.com/babysteps/blog/2019/10/26/async-fn-in-traits-are-hard/
+#[test]
+fn async_trait_method() {
+    struct H1RefFuture;
+
+    impl<'a> PlugLifetime<'a> for H1RefFuture {
+        type T = RefFuture<'a, usize>;
+    }
+
+    // This is the future that is returned by the implementation. This has to
+    // be a named struct instead of an anonymous `impl Future`/`async` block
+    // because it's referred by the `H1RefFuture` HKT form
+    struct RefFuture<'a, T>(&'a T);
+
+    impl<'a, T> Future for RefFuture<'a, T> {
+        type Output = &'a T;
+
+        fn poll(self: Pin<&mut Self>, _: &mut Context) -> Poll<Self::Output> {
+            Poll::Ready(self.0)
+        }
+    }
+
+    trait AsyncTrait {
+        type H1NonTrivialFuture: for<'a> PlugLifetime<'a>;
+
+        // This is basically the async identity function. The lifetime argument
+        // is the non-trivial parts of this example
+        fn non_trivial<'a>(
+            &self,
+            x: &'a usize,
+        ) -> <Self::H1NonTrivialFuture as PlugLifetime<'a>>::T
+        // By all rights this bound should be part of the declaration of
+        // `H1NonTrivialFuture` or a separate associated type
+        // `NonTrivialFuture`. Since `for<T>` is not a thing, we have to make
+        // do with this
+        where
+            <Self::H1NonTrivialFuture as PlugLifetime<'a>>::T:
+                Future<Output = &'a usize>;
+    }
+
+    struct AsyncTraitImpl;
+
+    impl AsyncTrait for AsyncTraitImpl {
+        type H1NonTrivialFuture = H1RefFuture;
+
+        fn non_trivial<'a>(
+            &self,
+            x: &'a usize,
+        ) -> <Self::H1NonTrivialFuture as PlugLifetime<'a>>::T
+        where
+            <Self::H1NonTrivialFuture as PlugLifetime<'a>>::T:
+                Future<Output = &'a usize>,
+        {
+            RefFuture(x)
+        }
+    }
+
+    let executor = LocalExecutor::new();
+    let payload = 42_usize;
+    let future = AsyncTraitImpl.non_trivial(&payload);
+
+    assert_eq!(executor.run(future), &payload);
 }
